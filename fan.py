5fc39f51c41043e6a9edc7dd055f7131ec4af3d1
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-09-16 19:22:44 +0300

    Add RaspiCM4IOBoardFanSensor that inherits EMC2301 and adds easier methods for fan speed reading and control

--- fan.py
+++ fan.py
@@ -1,36 +1,24 @@
-from time import sleep
+from types import ModuleType
 
-import smbus
+from i2c_pkg import i2c as i2c_module
 from i2c_pkg.emc2301_pkg import emc2301
-from i2c_pkg.emc2301_pkg import fan_type
 
 
-def fan_testing():
-    bus = smbus.SMBus(10)
-    address = 0x2f
+class RaspiCM4IOBoardFanSensor(emc2301.EMC2301):
+    def __init__(self,
+                 address: int = 0x2f,
+                 busnum: int = 10,
+                 i2c: ModuleType = i2c_module,
+                 **kwargs):
+        super().__init__(address=address,
+                         busnum=busnum,
+                         i2c=i2c,
+                         **kwargs)
 
-    sensor = emc2301.EMC2301(address=0x2f,
-                             busnum=10)
+    def fan_speed(self) -> int:
+        return self.speed()[0]
 
-    print('Running sensor self test...')
-    self_test_status = 'PASS' if sensor.self_test() == 0 else 'FAIL'
-    print(f'Status: {self_test_status}')
-    print(f'Product id: {sensor.productid()}')
-    print(f'Manufacturer id: {sensor.manufid()}')
-    print(f'Revision id: {sensor.revisionid()}')
-    print(f'Register list:')
-    print(emc2301.conf_register_list())
-    print('Set fan speed to 0')
-    sensor.write_register(register='FAN_SETTING', value=0)
-    for i in range(10):
-        print(f'Fan speed: {sensor.speed()[0]}')
-        sleep(1)
-    sensor.write_register(register='FAN_SETTING', value=255)
-    for i in range(10):
-        print(f'Fan speed: {sensor.speed()[0]}')
-        sleep(1)
-    sensor.write_register(register='FAN_SETTING', value=100)
-
-
-if __name__ == '__main__':
-    fan_testing()
+    def set_fan_speed_percentage(self, percentage: int):
+        """Percentage should be 0 - 100"""
+        converted_value = int(percentage / 100 * 255)
+        self.write_register(register='FAN_SETTING', value=converted_value)
